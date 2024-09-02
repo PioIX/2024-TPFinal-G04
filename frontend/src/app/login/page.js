0a4065585c1eae5e18d8b32de95874907582b118
FacundoJara <163416909+FacundoJara@users.noreply.github.com>
2024-09-02 10:04:31 -0300

    Primero

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,8 +1,25 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import clsx from "clsx"
+import Input from "@/components/input";
+import { useState } from "react";
 export default function Home() {
   var val=true
+  
+  
+  function spacepress(event) {
+    for(var i=0; i<event.target.value.length;i++ ){
+      if (i==" "){
+        console.log(event.target.value.split(i,1,""))
+        event.target.value.split(i,1,"")
+      }
+    }
+    return
+  }
+    
+  /*return (event.charCode != 32)*/ 
+  
   return (
     
     <main className={styles.main}>
@@ -10,11 +27,12 @@ export default function Home() {
             <h1>Preguntados</h1>
             <br></br>
             <label id="usernamecss" for="username">Nombre de usuario (No mas de 10 caracteres):</label>
-            <input onkeypress="return (event.charCode != 32)" placeholder="Nombre" type="text" id="username" maxlength="10"></input>
+            <Input onkeypress={spacepress} placeholder="Nombre" type="text" id="username" maxlength="10" ></Input>
             <br></br>
             <label id="passwordcss" for="password">Contraseña (No mas de 10 caracteres):</label>
-            <input  onkeypress="return (event.charCode != 32)" placeholder="Contraseña" type="text" id="password" maxlength="10" minlength="3"></input>
+            <Input onkeypress={spacepress} placeholder="Contraseña" type="text" id="password" maxlength="10" ></Input>
             <br></br>
+            
         </div>
         <div id="loginbuttons">
             <button id="ingresar" type="button" onclick="login()">Ingresar</button>
