077da13f458eef996f89b5ece52224153a5cf801
FacundoJara <163416909+FacundoJara@users.noreply.github.com>
2024-09-21 16:24:43 -0300

    segundojuego

--- frontend/src/components/sgame.js
+++ frontend/src/components/sgame.js
@@ -1,7 +1,7 @@
 "use client"
 import Button from "@/components/button";
 import { useState, useEffect } from "react";
-import styles from "./Button.module.css"
+import styles from "./Sgame.module.css"
 
 
 function getRandomInt(min, max) {
@@ -12,75 +12,87 @@ function getRandomInt(min, max) {
 
 export default function Naval(props) {
 
-    var boardSize=7
+
+	const manejarClick = (e) => {
+		// `e.target` es el elemento que fue clicado
+		const elementoClicado = e.target;
+		const idElemento = elementoClicado.getAttribute('id');
+		console.log(idElemento)
+		console.log(document.getElementById(idElemento))
+		console.log(document.getElementById(idElemento).getAttribute('class'))
+		document.getElementById(idElemento).setAttribute('class', 'Sgame_selected__hF5Ep')
+
+	  };
+    
     return(
     <div id="board">
 		<div id="messageArea"></div>
-		<table>
+		<table >
 		<tbody>
 			<tr>
-				<td><div id="00"></div></td>
-				<td><div id="01"></div></td>
-				<td><div id="02"></div></td>
-				<td><div id="03"></div></td>
-				<td><div id="04"></div></td>
-				<td><div id="05"></div></td>
-				<td><div id="06"></div></td>
+				
+				<td onClick={manejarClick} ><div id="0" className={styles.selected}></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="1"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="2"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="3"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="4"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="5"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="6"></div></td>
 			</tr>
 			<tr>
-				<td><div id="10"></div></td>
-				<td><div id="11"></div></td>
-				<td><div id="12"></div></td>
-				<td><div id="13"></div></td>
-				<td><div id="14"></div></td>
-				<td><div id="15"></div></td>
-				<td><div id="16"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="7"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="8"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="9"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="10"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="11"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="12"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="13"></div></td>
             </tr>
 			<tr>
-				<td><div id="20"></div></td>
-				<td><div id="21"></div></td>
-				<td><div id="22"></div></td>
-				<td><div id="23"></div></td>
-				<td><div id="24"></div></td>
-				<td><div id="25"></div></td>
-				<td><div id="26"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="14"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="15"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="16"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="17"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="18"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="19"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="20"></div></td>
 			</tr>
 				
 			<tr>
-				<td><div id="30"></div></td>
-				<td><div id="31"></div></td>
-				<td><div id="32"></div></td>
-				<td><div id="33"></div></td>
-				<td><div id="34"></div></td>
-				<td><div id="35"></div></td>
-				<td><div id="36"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="21"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="22"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="23"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="24"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="25"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="26"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="27"></div></td>
 			</tr>
 			<tr>
-				<td><div id="40"></div></td>
-				<td><div id="41"></div></td>
-				<td><div id="42"></div></td>
-				<td><div id="43"></div></td>
-				<td><div id="44"></div></td>
-				<td><div id="45"></div></td>
-				<td><div id="46"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="28"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="29"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="30"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="31"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="32"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="33"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="34"></div></td>
 			</tr>
 			<tr>
-				<td><div id="50">A</div></td>
-				<td><div id="51"></div></td>
-				<td><div id="52"></div></td>
-				<td><div id="53"></div></td>
-				<td><div id="54"></div></td>
-				<td><div id="55"></div></td>
-				<td><div id="56"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="35"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="36"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="37"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="38"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="39"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="40"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="41"></div></td>
 			</tr>
 			<tr>
-				<td><div id="60"></div></td>
-				<td><div id="61"></div></td>
-				<td><div id="62"></div></td>
-				<td><div id="63"></div></td>
-				<td><div id="64"></div></td>
-				<td><div id="65"></div></td>
-				<td><div id="66"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="42"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="43"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="44"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="45"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="46"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="47"></div></td>
+				<td className={styles.td} onClick={manejarClick}><div id="48"></div></td>
 			</tr>
             </tbody>
 		</table>
