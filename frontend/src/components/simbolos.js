17861d99d939cba9a9b074282888675482d6b455
florrr46 <141520251+florrr46@users.noreply.github.com>
2024-10-30 17:04:03 +0100

    masSimbolos

--- frontend/src/components/simbolos.js
+++ frontend/src/components/simbolos.js
@@ -11,53 +11,122 @@ function getRandomInt(min, max) {
 }
 
 export default function Simbolos(props) {
-    let [num1, setNum1] = useState(0);
-    let [num2, setNum2] = useState(0);
-    let [number1, setNumber1] = useState(0);
-    let [number2, setNumber2] = useState(0);
-    let [number3, setNumber3] = useState(0);
+    let [simboloEl, setSimboloEl] = useState([]);
+    let [simboloCom, setSimboloCom] = useState([]);
+	let started = false;
     const {socket,isConnected}=useSocket();
-    let started = false;
 
-    //envia
-    /*function handleClick(){
-        socket.emit("numeros",{numero: 8})
-      }*/
-        
-      //recibe
       useEffect(() => {
-
-        localStorage.setItem("misSimbolos", auxtotal2);
+        if (localStorage.getItem("userId") == 1) {
+            localStorage.setItem("miSimbolo", JSON.stringify(simboloEl));
+        }
         if (!socket) return;
-        socket.on('newSimbolo', (data)=>{
-            if (data.message.user != localStorage.getItem("userId")) { 
-                localStorage.setItem("respuestaSuya", data.message.numero);
+        socket.on('newSimbolo', (data) => {
+            if (localStorage.getItem("userId") == 2) {
+                localStorage.setItem("miSimbolo", JSON.stringify(data.message.position));
+                setSimboloEl(data.message.position);
             }
-          });
+        });
+        socket.on('newSimboloState', (data) => {
+            if (localStorage.getItem("userId") == 2) {
+                if(data.message.win=="lose"){
+                    console.log("perdiste")
+                }else{
+                    console.log("ganaste")
+                }
+            }
+        });
 
         if (!started) {
-            socket.emit("joinRoom",{room: "Kaboom"})
-            socket.emit("simbolo",{numero: auxtotal2,
-                user:localStorage.getItem("userId")
-            })
-            started=true
+            socket.emit("joinRoom", { room: "Kaboom" })
+            if (localStorage.getItem("userId") == 1) {
+                socket.emit("simbolo", { position: simboloCom })
+            }
+            started = true
         }
     }, [socket, isConnected])
 
+    function selectRandomSimbol(){
+        var positions = [];
+	    var number = 1;
+
+        for (let index = 0; index < 9; index++) {
+            number = getRandomInt(1, 28);
+            do {
+                number = getRandomInt(1, 28);
+            } while (positions.includes(number));
+            positions.push(number)
+
+        }
+        return positions
+    }
+    function shuffle(array) {
+        let currentIndex = array.length;
+
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+
+            // Pick a remaining element...
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+    }
+    useEffect(() => {
+        if (localStorage.getItem("userId") == 1) {
+            var a=[]
+            var copySimbol=[].concat(simboloEl)
+            shuffle(copySimbol)
+            for (let i = 0; i < 5; i++) {
+                 a.push(copySimbol[i])
+            }
+            setSimboloCom(a)
+        }
+
+    }, [simboloEl])
 
 
 
     useEffect(() => {
-        setNumber1(getRandomInt(1, 10))
-        setNumber2(getRandomInt(1, 10))
-        setNumber3(getRandomInt(1, 10))
+        if (localStorage.getItem("userId") == 2 && localStorage.getItem("miSimbolo") != undefined) {
+            setSimboloEl(JSON.parse(localStorage.getItem("miSimbolo")))
+        }
+        if (localStorage.getItem("userId") == 1) {
+            setSimboloEl(()=>selectRandomSimbol())
+            
+        }
+        if (localStorage.getItem("userId") == 2 && localStorage.getItem("miSimbolo") != undefined) {
+            if (JSON.parse(localStorage.getItem("miSimbolo"))[0].length > 3) {
+                setLista(JSON.parse(localStorage.getItem("miSimbolo")));
+            }
+        }
+
     }, [])
-    if (localStorage.getItem("userId")) {
-        
+
+    function revelar(){
+        console.log(simboloEl)
+        console.log(simboloCom)
     }
-    return (
-        <div className={styles.all}>
-        
-        </div>
-    )
-}
+
+
+    if (localStorage.getItem("userId")==1) {
+        return (
+            <div className={styles.all}>
+                <Button onClick={revelar} text="reveal"></Button>
+             <h1>{simboloEl}</h1>
+             <h1>{simboloCom}</h1>
+            </div>
+        )
+    }
+    if (localStorage.getItem("userId")==2) {
+        return (
+            <div className={styles.all}>
+                <Button onClick={revelar} text="reveal"></Button>
+             <h1>{simboloEl}</h1>
+            </div>
+        )
+    }
+    }
