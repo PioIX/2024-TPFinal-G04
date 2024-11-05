1f5415de0002a915e40776f5838b65d5a0e762ac
MaiaBelenSilva <164505625+MaiaBelenSilva@users.noreply.github.com>
2024-11-05 08:39:21 -0300

    maia

--- frontend/src/components/sgame.js
+++ frontend/src/components/sgame.js
@@ -118,7 +118,7 @@ export default function Naval(props) {
 	return (
 		<div id="board">
 			<div id="messageArea" className={styles.all}>
-				<Image src={luzcomponente} alt="componente1" width={80} height={80} ></Image>
+				<Image className={styles.lucesita} src={luzcomponente} alt="componente1" width={80} height={80} ></Image>
 				<table  className={styles.todo}>
 					<tbody className={styles.fondo}>
 						<tr>
